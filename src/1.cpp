8a4a0c3cf0ef90d2b2c3923f4b6704c3c9ec0bf1
ginette4mb <ginette4mb@gmx.com>
2025-03-04 12:39:27 +0000

    Add 1.cpp

---
+++ src/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+ int n=0;
+ cout<<"Enter a number";
+ cin>>n;
+ if(n%2==0) {
+  cout<<"Number is even";
+ }else{
+  cout<<"Number is odd";
+ }
+ return 0;
+}
